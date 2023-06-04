c240860ecc388cd19378a7efeb77f822964bc45b
GiannosL <giannos.louloudis@gmail.com>
2023-06-04 16:16:41 +0200

    jojo names

---
+++ source/__init__.py
@@ -0,0 +1,13 @@
+# Jojo parts
+JOJO_PART_NAMES = {
+    'Phantom blood': 'part_1', 
+    'Battle tendency': 'part_2', 
+    'Stardust crusaders': 'part_3',
+    'Diamond is unbreakable': 'part_4', 
+    'Golden wind': 'part_5', 
+    'Stone ocean': 'part_6',
+    'Steel ball run': 'part_7', 
+    'Jojolion': 'part_8', 
+    'The Jojolands': 'part_9', 
+    '': None
+    }
